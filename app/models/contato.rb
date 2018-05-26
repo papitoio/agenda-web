a46a39e57f53a029b5d2714237b70bc7aa369bf8
Devops Ninja <devops@qaninja.io>
2018-05-26 11:49:40 -0300

    ajustando o bug de mensagem quando o usuario nao informa o nome

--- app/models/contato.rb
+++ app/models/contato.rb
@@ -1,4 +1,4 @@
 class Contato < ApplicationRecord
     validates :whats, uniqueness: true
-    validates_presence_of :nome, menssage: 'Ops. O nome do contato deve ser preenchido'
+    validates_presence_of :nome, message: 'Ops. O nome do contato deve ser preenchido'
 end
